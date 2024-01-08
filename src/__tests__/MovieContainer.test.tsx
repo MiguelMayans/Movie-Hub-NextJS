45a0e58dad3a5f3749829464b97cbcef2e47ee1c
Miguel Mayans <miguelmayansv@gmail.com>
2024-01-08 10:26:54 +0100

    fix: eslint erros on tests

--- src/__tests__/MovieContainer.test.tsx
+++ src/__tests__/MovieContainer.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+// import { render, screen } from "@testing-library/react";
 import { describe, test, expect } from "vitest";
 import MovieContainer from "../components/MovieContainer/MovieContainer";
 
